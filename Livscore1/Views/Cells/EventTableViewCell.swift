7bc65d434a2e34ff8148f3f36314cc0e7ca62200
Carlos Cardona <cardonacarlos97.06@gmail.com>
2022-05-12 23:11:49 -0500

    Liv-16 (FixDetailViewController)
    - created `FixDetailViewController`
    - add assets
    - Start with the UI of `FixDetailViewController`

---
+++ Livscore1/Views/Cells/EventTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventTableViewCell.swift
+//  Livscore1
+//
+//  Created by Carlos Cardona on 12/05/22.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
